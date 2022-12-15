6a596fbb9c11a130ad11499b070bed4650f13465
AlexTRDev <104025084+AlexTRDev@users.noreply.github.com>
2022-12-14 19:07:29 -0500

    Rutas - config
    instalar las dependecias de react-router-dom y styled components

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,12 @@ import { store } from "./redux/store/store";
 import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
